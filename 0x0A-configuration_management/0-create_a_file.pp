15e395e248d193ce9f4641987ac221045d527425
AbdelazizAdrehmi <122813821+AbdelazizAdrehmi@users.noreply.github.com>
2024-07-03 20:38:42 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in /tmp
-
+# Creates a file
 file { '/tmp/school':
-  content =>'I love Puppet',
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
